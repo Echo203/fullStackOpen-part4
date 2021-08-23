a571c71fbb0ca3759a7cb869e758984f35ef86fe
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-08-23 11:14:43 +0200

    done excercises including 4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,36 @@
+const totalLikes = (listOfBlogPosts) => {
+  if (listOfBlogPosts.length === 0) {
+    return 0;
+  } else {
+    const sumOfLIkes = listOfBlogPosts.reduce((sum, post) => {
+      return sum + post.likes;
+    }, 0);
+    return sumOfLIkes;
+  }
+};
+
+const favoriteBlog = (listOfBlogPosts) => {
+  let mostLikedPost = listOfBlogPosts[0]
+  listOfBlogPosts.forEach((post) => {
+		console.log(`post`, post)
+		console.log(`mostLikedPost`, mostLikedPost)
+		if (post.likes >= mostLikedPost.likes) {
+      mostLikedPost = post;
+    }
+  });
+  return {
+    title: mostLikedPost.title,
+    author: mostLikedPost.author,
+    likes: mostLikedPost.likes
+  };
+};
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+module.exports = {
+  dummy,
+	favoriteBlog,
+  totalLikes,
+};
