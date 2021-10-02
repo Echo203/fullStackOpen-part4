1dd0666b012535c52b4558bcfe19762580440ed4
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-02 13:55:25 +0200

    all excercises till 4.12

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,10 +1,10 @@
-const errorHandler = (error, request, response) => {
+const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   }
-
+  next(error)
 };
 
 const unknownEndpoint = (request, response) => {
