195143d7bed0a3cd1c36d1cf0c9d455a6d614b7e
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-14 15:38:57 +0200

    Applying JWTwith login route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const userRouter = require('./controllers/userRouter')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -15,6 +16,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/blogRouter.js
+++ controllers/blogRouter.js
@@ -1,41 +1,58 @@
-const blogRouter = require("express").Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogRouter.get('/', async (request, response) => {
-    const posts = await Blog.find({}).populate('user', { username: 1, name: 1 })
-    response.json(posts)
-  })
-  
-  blogRouter.post('/', async (request, response) => {
-    const user = await User.findOne({})
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.toLowerCase().startsWith('baerer ')){
+    return authorization.substring(7)
+  }
+  return null
+}
 
-    const blogObject = new Blog({
-      title: request.body.title,
-      author: request.body.author,
-      url: request.body.url,
-      likes: request.body.likes,
-      user: user._id
-    })
-    
-    const savedBlog = await blogObject.save()
-    response.status(201).json(savedBlog)
- 
-    
-  })
+blogRouter.get("/", async (request, response) => {
+  const posts = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(posts);
+});
 
-blogRouter.delete('/:id', async (req, res) => {
-  await Blog.findByIdAndRemove(req.params.id)
-  res.status(204).end()
-})
-
-blogRouter.put('/:id', async (req, res) => {
-  const blogPost = {
-    likes: req.body.likes
+blogRouter.post("/", async (request, response) => {
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if(!token || !decodedToken.id) {
+    return response.status(400).json({ error: 'invalid or missing token' })
   }
 
-  const updatedBlogPost = await Blog.findByIdAndUpdate(req.params.id, blogPost, {new: true})
-  res.json(updatedBlogPost)
-})
+  const user = User.findById(decodedToken.id)
 
-module.exports = blogRouter
+  const blogObject = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+    user: user._id,
+  });
+
+  const savedBlog = await blogObject.save();
+  response.status(201).json(savedBlog);
+});
+
+blogRouter.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id);
+  res.status(204).end();
+});
+
+blogRouter.put("/:id", async (req, res) => {
+  const blogPost = {
+    likes: req.body.likes,
+  };
+
+  const updatedBlogPost = await Blog.findByIdAndUpdate(
+    req.params.id,
+    blogPost,
+    { new: true }
+  );
+  res.json(updatedBlogPost);
+});
+
+module.exports = blogRouter;
